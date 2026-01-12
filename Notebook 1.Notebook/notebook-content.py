d3c10c97716d269aab35d07a31c71ff58a28968f
Takele2021 <81451453+Takele2021@users.noreply.github.com>
2026-01-12 12:38:09 -0500

    Committing 15 items from workspace 9e7137d9-86ac-49ed-a6bf-cafe88d880d8

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,49 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a2cbd6c3-f387-4f22-9c45-ed2d1fde299e",
+# META       "default_lakehouse_name": "Take_lake_house",
+# META       "default_lakehouse_workspace_id": "9e7137d9-86ac-49ed-a6bf-cafe88d880d8",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a2cbd6c3-f387-4f22-9c45-ed2d1fde299e"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+import pandas as pd
+
+wrangler_sample_df = pd.read_csv("https://aka.ms/wrangler/titanic.csv")
+display(wrangler_sample_df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark",
+# META   "editable": true
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ TakeWareHouse.Warehouse/Gold/Tables/crm_cust_info.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Gold].[crm_cust_info] (
+
+	[cst_id] int NULL, 
+	[cst_key] varchar(50) NULL, 
+	[cst_firstname] varchar(50) NULL, 
+	[cst_lastname] varchar(50) NULL, 
+	[cst_marital_status] varchar(50) NULL, 
+	[cst_gndr] varchar(50) NULL, 
+	[cst_create_date] date NULL, 
+	[dwh_create_date] datetime2(6) NULL
+);
